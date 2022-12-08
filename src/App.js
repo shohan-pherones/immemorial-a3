7f13d45731fb8e9dbbda572a6149e1f723082618
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-08 22:21:16 +0600

    s scrolls added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { useSmoothScroll } from "./hooks/useSmoothScroll";
 
 const App = () => {
+  useSmoothScroll();
+
   return (
     <div>
       <Navbar />
