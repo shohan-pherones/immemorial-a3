4ac7180f9b85d72be395f7826c333b5e06dc35d6
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-10 22:53:22 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import { useSmoothScroll } from "./hooks/useSmoothScroll";
+import Footer from "./components/Footer";
+import Featured from "./components/Featured";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Blog from "./components/Blog";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   useSmoothScroll();
@@ -11,7 +17,13 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/featured" element={<Featured />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/blog" element={<Blog needFullHeight={true} />} />
+        <Route path="*" element={<NotFound needFullHeight={true} />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
